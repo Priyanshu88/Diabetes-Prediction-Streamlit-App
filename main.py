e10bc37f6224ed0f7f2fd5b34e362cf2b7dafb49
PRIYANSHU PANDEY <2040020@sliet.ac.in>
2023-04-14 21:12:16 +0530

    refactor

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import StandardScaler
+# import plotly.express as px
+# from plotly.subplots import make_subplots
+# import plotly.graph_objects as go
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.preprocessing import StandardScaler
 import pickle
 
 pickle_in = open('Diabetes.pkl', 'rb')
