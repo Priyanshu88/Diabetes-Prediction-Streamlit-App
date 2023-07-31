24d20ecde2a9b6d68b07229cdf576b095695f8bd
Priyanshu88 <2040020@sliet.ac.in>
2023-07-31 19:33:46 +0530

    link change

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def main():
     read_me = st.markdown("""
     The application is built using Streamlit  
     to demonstrate Diabetes Prediction. It performs prediction on multiple parameters
-    [here](https://github.com/).""")
+    [here](https://github.com/Priyanshu88/Diabetes-Prediction-Streamlit-App#signal_strength-dataset).""")
     st.sidebar.title("Select Activity")
     choice = st.sidebar.selectbox(
         "MODE", ("About", "Predict Diabetes"))
